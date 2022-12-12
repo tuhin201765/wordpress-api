dc7fb19052b2ff3a819ccdb2293eab346fe1e73c
tuhin201765 <tuhin201765@gmail.com>
2022-12-12 13:24:47 -0800

    first commit

---
+++ pixabay image url to text file.py
@@ -0,0 +1,17 @@
+from requests import get
+from pprint import pprint
+query = 'yellow+flowers'
+api_key ='32029211-b9a80ab832d0119c08a0b3979'
+url = f'https://pixabay.com/api/?key={api_key}&q={query}&image_type=photo&pretty=true'
+r = get(url)
+api_data = r.json().get('hits')
+pprint(api_data)
+# res = api_data[0]
+# pprint(res)
+for data in api_data:
+    webformatURL = data.get('webformatURL')
+    largeImageURL = data.get('largeImageURL')
+    file = open('Pixabay img to text.txt', 'a+')
+    file.writelines(largeImageURL + '\n')
+    file.close()
+    # print(image_url)
